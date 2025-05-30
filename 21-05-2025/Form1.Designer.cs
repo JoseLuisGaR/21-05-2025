4066ba5585521ba1615892c24fb1dc86c103074b
joyit <joyit@DESKTOP-RLKKRMN>
2025-05-30 17:29:14 -0600

    Agregar archivos de proyecto.

---
+++ 21-05-2025/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace _21_05_2025
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtMatricula = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtCarrera = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(12, 17);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(75, 23);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Matricula";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Carrera";
+            // 
+            // txtMatricula
+            // 
+            this.txtMatricula.Location = new System.Drawing.Point(121, 38);
+            this.txtMatricula.Name = "txtMatricula";
+            this.txtMatricula.Size = new System.Drawing.Size(100, 20);
+            this.txtMatricula.TabIndex = 4;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(121, 90);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtCarrera
+            // 
+            this.txtCarrera.Location = new System.Drawing.Point(121, 139);
+            this.txtCarrera.Name = "txtCarrera";
+            this.txtCarrera.Size = new System.Drawing.Size(100, 20);
+            this.txtCarrera.TabIndex = 6;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(233, 12);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtCarrera);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtMatricula);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConectar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtMatricula;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtCarrera;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ 21-05-2025/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Protobuf.WellKnownTypes;
+using IronXL;
+
+namespace _21_05_2025
+{
+    public partial class Form1 : Form
+    {
+        string ruta = AppDomain.CurrentDomain.BaseDirectory;
+        int rowl = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ruta += "file.xlsx";
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            WorkBook workBook = WorkBook.Load(@ruta);
+            WorkSheet workSheet = workBook.WorkSheets.First();
+
+            int cellValue = workSheet["A2"].IntValue;
+            foreach (var cell in workSheet["A1:C5"])
+            {
+                MessageBox.Show(cell.AddressString + " " + cell.Text);
+            }
+            decimal sum = workSheet["A2:A5"].Sum();
+            MessageBox.Show(sum.ToString());
+
+            decimal max = workSheet["A2:A5"].Max();
+            MessageBox.Show(max.ToString());
+            int i = 10;
+            workSheet["A"+i.ToString()].Value = "8";
+
+            workSheet.SetCellValue(1, 1, "prueba");
+            workSheet.SaveAs(@ruta);
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            WorkBook workBook = WorkBook.Load(@ruta);
+            WorkSheet workSheet = workBook.WorkSheets.First();
+             if(txtMatricula.Text == "")
+            {
+                MessageBox.Show("Ingrese el valor de Matricula"); 
+                txtMatricula.Focus();
+            }
+            else if (txtNombre.Text == "")
+            {
+                MessageBox.Show("Ingrese el valor de Nombre");
+                txtNombre.Focus();
+            }
+            else if (txtCarrera.Text == "")
+            {
+                MessageBox.Show("Ingrese el valor de Carrera");
+                txtCarrera.Focus();
+            }
+            else
+            {
+                workSheet.SetCellValue(rowl,1 , txtMatricula);
+                workSheet.SetCellValue(rowl, 1, txtNombre);
+                workSheet.SetCellValue(rowl, 1, txtCarrera);
+                rowl++;
+                txtMatricula.Clear();
+                txtCarrera.Clear();
+                txtNombre.Clear();
+            }
+
+                rowl++;
+            workSheet.SaveAs(@ruta);
+        }
+    }
+}
